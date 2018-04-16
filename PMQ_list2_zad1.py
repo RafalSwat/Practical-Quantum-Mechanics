b3f1b7ef5e6aabd014a14a71dccf457814f5cf84
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-04-16 18:49:38 +0200

    Update PMQ_list2_zad1.py

--- PMQ_list2_zad1.py
+++ PMQ_list2_zad1.py
@@ -1,75 +1,80 @@
-# deklaracja bibliotek
-import numpy as np
 import matplotlib.pyplot as plt
+
+# needs explicit subpackage import
 from scipy.integrate import odeint
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, brentq
+import numpy as np
+import RKMethods
 
-# declaration of variables
-me = 9.109383e-31   # kg  
-hbar = 1.053571e-34 # Js
-eV = 1.602176e-19   # J
-h = 1e-12           # krok 
-mw = me * 0.067     # masa w studni
-mb = me * 0.1       # masa w barierze
-V = 0.334 * eV      # potencjal
-E0 = 0.1 * eV       # energia czastki
-b = 10 * 1e-9       # szerokosc bariery
-w = 10 * 1e-9       # szerokosc studni
-fV = []             # wartosci bariery do wykresu
-psi = []            # warosci f. falowej
-ksi = []            # wartosci f.pomocniczej
-iterator = []       # war. iteratora
-psi.append(0)       # war. poczatkowe psi[0]=0
-ksi.append(1)       # war. poczatkowe ksi[0]=0
-iterator.append(0)  # war. poczatkowe i[0]=0
-fV.append(V)        # war. poczatkowe fV[0]=0
+w = 4.0
+a = -w*0.5
+b = w*0.5
 
-# definiuje metode strzalow
-def f_psi(E):
-    h = 1e-12
-    mw = me * 0.067 
-    mb = me * 0.1
-    hbar = 1.053571e-34
-    
-    for i in range(1,100):
-        if 30 < i < 70:
-            iterator.append(z * 10 * 10e-9)
-            psi.append(psi[i-1] * h * mw * ksi[i-1])
-            ksi.append(ksi[i-1] * h * (2.0/hbar) * (0 - E) * psi[i-1])
-            fV.append(0)
-        else:
-            iterator.append(z * 10 * 10e-9)
-            psi.append(psi[i-1] * h * mb * ksi[i-1])
-            ksi.append(ksi[i-1] * h * (2.0/hbar) * (V - E) * psi[i-1])
-            fV.append(V)
-        return psi 
-    
-# definiuje pochodna psi
-def df_psi(x):
-    h = 1e-12 
-    xh=x*h
-    return 0.5*(f_psi(x+xh)-f_psi(x-xh))/xh
+N = 500
+z = np.linspace(a, b, N)
 
-# definiuje metode Newtona
-def Newton(x):
-    eps = 10e-14
-    x1 = x
-    x0 = 0
-    i = 0
-    lmax = 500
-    while np.fabs((x0 - x1)/x1) and (i < lmax):
-        x0 = x1
-        x1 = x0 - f_psi(x0)/df_psi(x0)
-        i = i + 1
-    if i < lmax:
-        return x1
-    else:
-        return -100.0
-    
-# szukanie warosci wlasnych
-max = 10e7
-E0 = 0.01
-for i in range(1, 334):
-    E0 = E0 + i/1000
-    Ew = Newton(E0)
-    print(Ew)
+psi_0 = 0.
+psit_0 = 1.
+
+#search range
+E = np.linspace(0, 50, 1000)
+#potential
+V = np.zeros(N)
+
+#effective mass
+m0 = 0.067
+m = m0 * np.ones(N)
+
+
+def calc_psi(z, psi, psit, V, m, E):
+    dz = z[1]-z[0]
+    for i in range(1, len(z)):
+        psi[i] = psi[i-1] + dz*m[i]*psit[i-1]
+        psit[i] = psit[i-1] + dz * 2.0 * (V[i] - E) * psi[i-1]
+    return psi
+
+
+psi = np.zeros(N)
+psit = np.ones(N)
+
+e_prev = 0.0
+de_eps = 0.1
+solutions = []
+# Dla zadanego z góry spektrum energii
+# sprawdzamy czy ktoras wartosc jest taka ze da 0 na drugim brzegu.
+for e in E:
+    p = calc_psi(z, psi, psit, V, m, e)
+
+    previous_psi = p[-2]
+    last_psi = p[-1]
+    '''
+    Filtrowanie danych.
+    '''
+    # Po pierwsze sprawdzamy czy zmienia się znak dwóch ostatnich wartości
+    # Jesli tak to znaczy że "przeszło" przez 0
+    if np.sign(previous_psi) + np.sign(last_psi) == 0:
+        # Po drugie nie chcemy aby notowac kilkukrotnie te samy wartości
+        de = e - e_prev
+        if de > de_eps:
+            print(previous_psi, last_psi, e, de)
+            #kopia bo potem zmieniamy to co jest pod p (a to numpy ;))
+            solutions.append( (e, p.copy()))
+        e_prev = e
+
+
+for s in solutions:
+    _psi = s[1]
+    dz = z[1] - z[0]
+    dpsi = np.gradient(_psi, dz)
+    e0 = s[0]
+    for i in range(1000):
+        e0 = e0 - _psi[-1]/dpsi[-1]
+        print (e0)
+
+for s in solutions:
+    plt.plot(z, s[1], label=r'E=%0.2f' % s[0])
+
+plt.grid()
+plt.title("Wave functions for infinite potential well")
+plt.legend()
+plt.show()
